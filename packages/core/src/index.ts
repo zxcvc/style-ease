c62081c3eecbd2f5dd538b614eb6af2e5fb9c7f1
zzz <978125451@qq.com>
2023-05-30 13:40:34 +0800

    reactor:core优化

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -10,7 +10,7 @@ async function transformHtml(html: string): Promise<string> {
 		const styles = $(`style[lang=${lang}]`);
 		for (let i = 0; i < styles.length; ++i) {
 			const style = styles[i];
-			const child = style.children[0] as unknown as Text;
+			const child = style.firstChild as unknown as Text;
 			const css = await to_css[lang](child.nodeValue || "");
 			child.nodeValue = css;
 			$(style).removeAttr("lang");
